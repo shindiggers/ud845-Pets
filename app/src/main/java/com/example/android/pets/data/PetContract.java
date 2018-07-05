8a5c97761c122c173093f3e646063505d997c05f
shindiggers <sean.mitchell666@gmail.com>
2018-07-05 13:46:22 +0100

    Add data validation checks to pet input

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -85,5 +85,12 @@ public final class PetContract {
 
         /** The content URI to access the pet data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
+
+        public static boolean isGenderValid (int gender){
+            if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE){
+                return true;
+            }
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -3,6 +3,7 @@ package com.example.android.pets.data;
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -140,6 +141,26 @@ public class PetProvider extends ContentProvider {
      * for that specific row in the database.
      */
     private Uri insertPet(Uri uri, ContentValues values) {
+
+        // Check that the name is not null
+        String name = values.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Pet requires a name");
+        }
+
+        // Check that the gender is valid
+        Integer gender = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_GENDER);
+        if(gender == null || !PetContract.PetEntry.isGenderValid(gender)){
+            throw new IllegalArgumentException("Pet gender cannot be null and must be either 0, 1 or 2");
+        }
+
+        // If the weight is provided, check that it's greater than or equal to 0 kg
+        Integer weight = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+        if(weight != null && weight < 0){
+            throw new IllegalArgumentException("Weight cannot be less than 0");
+        }
+
+        // No need to check the breed, any value is valid (including null).
 
         // Get writable database
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
